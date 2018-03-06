9ee5b2577f53a2985e4cdc47943b51b5b6af698e
tgialoimtr <tgialoimtr@gmail.com>
2018-03-06 14:14:05 +0700

    update configuration

--- src/common.py
+++ src/common.py
@@ -5,22 +5,23 @@ class DFO(object):
     pass
 
 args = DFO()
-args.model_path = '/home/loitg/debugtf/model_version4_total/'
-args.imgsdir = '/home/loitg/Downloads/complex-bg/'
-args.numprocess = 2
-args.qget_wait_count = 200
+args.model_path = '/home/loitg/location_nn/tfmodel/'
+args.imgsdir = ''
+args.numprocess = 2 #CPU:2 #GPU:8
+args.qget_wait_count = 400
 args.qget_wait_interval = 0.5
-args.bucket_size = 1
+args.bucket_size = 2 #CPU:2 #GPU:16 
 args.bucket_max_time = 10
 args.device = '/device:CPU:0'
 args.javapath = '/home/loitg/location_nn/java'
-args.dbfile = 'top20_1.csv'
+args.logsdir = '/home/loitg/location_nn/logs/'
+args.dbfile = 'top200_v3.csv'
 args.locationnjar = 'location_nn.jar'
 args.download_dir = '/home/loitg/location_nn/downloads'
 args.connection_string = 'DefaultEndpointsProtocol=http;AccountName=storacctcapitastartable;AccountKey=Z/dhpkNhR7DY0goHVsaPldFCnqzydIN/CunYh324E8M82eqOGeupYFS5CGz7CS18FDm1wWmWPEX3ecxJ23HqmA=='
-args.queue_get_name = 'loitg-queue-get'
-args.queue_push_name = 'loitg-queue-push'
-args.container_name = 'loitg-local'
-args.receipt_waiting_interval = 10 #seconds
-args.heartbeat_check = 500 #second
-args.mode = 'process' # delete, upload, process
+args.queue_get_name = 'ocr-receipt-queue-hangtest'
+args.queue_push_name = 'receipt-info-queue-hangtest'
+args.container_name = 'mobile-receipts-dev-hangtest'
+args.receipt_waiting_interval = 10
+args.heartbeat_check = 300
+args.mode = 'process'
