86b728f9d018440ad67c43a9f9c202cda4cc8483
tgialoimtr <tgialoimtr@gmail.com>
2018-06-04 16:15:51 +0700

    support test

--- src/accuracy_measure/flatten_imageshiarch.py
+++ src/accuracy_measure/flatten_imageshiarch.py
@@ -7,29 +7,60 @@ import os
 import sys
 import shutil
 
-def recursiveCopy(destination, outpath):
-    all_files = []
+def name2path(destination):
+    mapping = {}
     first_loop_pass = True
     for root, _dirs, files in os.walk(destination):
         if first_loop_pass:
             first_loop_pass = False
             continue
         for filename in files:
-            all_files.append(os.path.join(root, filename))
-            try:
-                shutil.copyfile(os.path.join(root, filename), os.path.join(outpath, filename))
-            except Exception:
-                print 'ERROR ' + filename
+            mapping[filename] = os.path.join(root, filename)
+    return mapping
 
+def recursiveCopy(destination, outpath):
+    mapping = name2path(destination)
+    for filename, path in mapping.iteritems():
+        try:
+            shutil.copyfile(path, os.path.join(outpath, filename))
+        except Exception:
+            print 'ERROR ' + filename
 if __name__ == '__main__':
     try:
         parts = sys.argv[1]
         outpath = sys.argv[2]
+        selected = sys.argv[3]
     except Exception:
-        print("parts, outputfolder")
+        print("parts, outputfolder, selected")
         sys.exit(0)
     print(parts)
     print(outpath)
+    print(selected)
     
-    recursiveCopy(parts, outpath)
+    mapping = name2path(parts)
+    for i, line in enumerate(open(selected, 'r')):
+        fn = line.split(',')[3].rstrip()
+        print fn
+        if fn not in mapping:
+            print 'NOT EXIST'
+            continue
+        path = mapping[fn]
+        print path
+        try:
+            shutil.copyfile(path, os.path.join(outpath, fn))
+        except Exception:
+            print 'ERROR ' + fn
+
+
+# if __name__ == '__main__':
+#     try:
+#         parts = sys.argv[1]
+#         outpath = sys.argv[2]
+#     except Exception:
+#         print("parts, outputfolder")
+#         sys.exit(0)
+#     print(parts)
+#     print(outpath)
+#     
+#     recursiveCopy(parts, outpath)
     
